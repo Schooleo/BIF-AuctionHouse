77837e20eada9f50cf4986fb201750e30fbc0045
Schooleo <lnnt190805@gmail.com>
2025-11-13 13:37:42 +0700

    Fixed initialization for running Docker

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import bcrypt from "bcryptjs";
+import bcrypt from "bcrypt";
 import crypto from "crypto";
 import { User } from "../models/user.model";
 import { generateToken } from "../utils/jwt.util";
