1614491c272797fc87dee4cea0a623a84303db26
Schooleo <89637415+Schooleo@users.noreply.github.com>
2025-11-15 17:38:26 +0700

    Revert "Added skeletons controller, routes and types files for backend development."

--- server/src/models/index.model.ts
+++
@@ -1,7 +0,0 @@
-export * from "./user.model";
-export * from "./category.model";
-export * from "./product.model";
-export * from "./bid.model";
-export * from "./rating.model";
-export * from "./watchlist.model";
-export * from "./upgradeRequest.model";
--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -3,7 +3,6 @@ import {
   register,
   login,
   requestPasswordReset,
-  resetPassword
 } from "../controllers/auth.controller";
 
 const router = Router();
@@ -11,6 +10,5 @@ const router = Router();
 router.post("/register", register);
 router.post("/login", login);
 router.post("/reset-password", requestPasswordReset);
-router.post("/reset-password/:token", resetPassword);
 
 export default router;
