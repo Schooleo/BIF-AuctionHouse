5d7557ce818fc9b3aa5936742a605b63ea1dee0c
vnquy126904 <ngocquyvan2005@gmail.com>
2025-11-15 22:23:46 +0700

    [Frontend] Add Button component

--- client/src/components/forms/Button.tsx
+++ client/src/components/forms/Button.tsx
@@ -1,5 +1,28 @@
-const Button = () => {
-  return <div>Button</div>;
-};
+import type { ButtonProps }  from "../../types/ui";
+
+const baseStyles = "bg-[#003366] text-white rounded-full p-3 font-medium transition-all duration-200 disabled:opacity-50 disabled:cursor-not-allowed focus:ring-2 focus:outline-none focus:ring-blue-500 hover:cursor-pointer";
+
+const Button: React.FC<ButtonProps> = ({
+  label = "Button",
+  disabled = false,
+  variant = 'primary',
+  className = '',
+  onClick,
+  ...rest
+}) => {
+  const Styles = {
+    primary: `${baseStyles} hover:bg-[#003333]`,
+    secondary: `${baseStyles} bg-opacity-60 hover:bg-opacity-80`,
+  }[variant]
+  return (
+    <button onClick={onClick} 
+      className={`${Styles} ${className}`} 
+      disabled={disabled} 
+      {...rest}
+      >
+        {label}
+    </button>
+  );
+}
 
 export default Button;
