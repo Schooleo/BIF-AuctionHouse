5d785c49b504aea7f712f2ca4ebee80552c780a2
vnquy126904 <ngocquyvan2005@gmail.com>
2025-11-15 22:26:20 +0700

    [Frontend] Add FormCard component

--- client/src/components/ui/FormCard.tsx
+++ client/src/components/ui/FormCard.tsx
@@ -1,5 +1,47 @@
-const FormCard = () => {
-  return <div>FormCard</div>;
-};
+import type { FormCardProps } from "../../types/ui";
+import InputField from "../forms/InputField";
+import PasswordField from "../forms/PasswordField";
+import Button from "../forms/Button";
 
-export default FormCard;
+const FormCard: React.FC<FormCardProps> = ({
+  title = "Form Pattern",
+  fields = [],
+  className = "",
+  buttonProps = {},
+  onSubmit
+}) => {
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    onSubmit && onSubmit();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}
+      className={`
+          bg-white p-8 rounded-xl shadow-lg flex flex-col gap-6
+          transition-all duration-300 ease-in-out hover:scale-[1.01] hover:shadow-2xl
+          ${className}
+      `}
+    >
+      <h2 className="text-2xl font-semibold">{title}</h2>
+        <div className="bg-white flex flex-col gap-10 mt-16">
+          {fields.map((field, index) => (
+            field.type == "password" ? (
+              <PasswordField
+                key={index}
+              {...field}
+            />
+          ) : (
+            <InputField
+              key={index}
+              {...field}
+            />
+          )
+        ))}
+          <Button className={'w-full mt-5'} {...buttonProps} />
+        </div>
+    </form>
+  )
+}
+
+export default FormCard
