60ab78b4b2c7835bfce1bab3e14f51a0548beb2d
vnquy126904 <ngocquyvan2005@gmail.com>
2025-11-16 18:09:00 +0700

    [Frontend] Add reset passwrod page and update route path to reset password page

--- client/src/containers/ResetPasswordContainer.tsx
+++ client/src/containers/ResetPasswordContainer.tsx
@@ -1,5 +1,92 @@
+import { useState } from "react";
+import FormCard from "../components/ui/FormCard";
+import ReCAPTCHA from "react-google-recaptcha";
+
 const ResetPasswordContainer = () => {
-  return <div>ResetPasswordContainer</div>;
+  const [email, setEmail]  = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [captchaToken, setCaptchaToken] = useState<string | null>(null);
+  const [error, setError] = useState<{email?: string, password?: string, confirmPassword?: string, captcha?: string}>({});
+
+  const handleSubmit = () => {
+    setError({});
+
+    const newErrors: {email?: string, password?: string, confirmPassword?: string, captcha?: string} = {};
+    
+    if (!email) newErrors.email = "Email is required";
+    if (!password) newErrors.password = "Password is required";
+    if (!confirmPassword) newErrors.confirmPassword = "Confirm Password is required";
+    if (!captchaToken) newErrors.captcha = "Please complete the reCAPTCHA";
+    
+    if (password !== confirmPassword) {
+        newErrors.confirmPassword = "Passwords do not match";
+    }
+
+    setError(newErrors);
+
+    if (Object.keys(newErrors).length == 0) {
+      console.log({
+        email,
+        password,
+        captchaToken,
+      });
+    }
+  };
+
+  return (
+      <FormCard
+        title="Reset Password"
+        className="p-6 shadow-lg flex flex-col gap-2"
+        fields={[
+          {
+            label: "Email",
+            type: "email",
+            value: email,
+            onChange: (e: React.ChangeEvent<HTMLInputElement>) =>
+              setEmail(e.target.value),
+            isRequired: true,
+            error: error.email,
+          },
+          {
+            label: "Password",
+            type: "password",
+            value: password,
+            onChange: (e: React.ChangeEvent<HTMLInputElement>) =>
+              setPassword(e.target.value),
+            isRequired: true,
+            error: error.password,
+          },
+          {
+            label: "Confirm Password",
+            type: "password",
+            value: confirmPassword,
+            onChange: (e: React.ChangeEvent<HTMLInputElement>) =>
+              setConfirmPassword(e.target.value),
+            isRequired: true,
+            error: error.confirmPassword,
+          },
+        ]}
+        buttonProps={{
+          label: "RESET PASSWORD",
+          type: "submit",
+          variant: "secondary",
+        }}
+        onSubmit={handleSubmit}
+      >
+        {/* reCAPTCHA */}
+        <div className="mt-5">
+          <ReCAPTCHA
+            sitekey={String(import.meta.env.VITE_RECAPTCHA_SITE_KEY)}
+            onChange={(token) => setCaptchaToken(token)}
+          />
+        </div>
+
+        {/* Error message */}
+        {error.captcha && <p className="text-red-500 mt-3">{error.captcha}</p>}
+      
+    </FormCard>
+  );
 };
 
 export default ResetPasswordContainer;
