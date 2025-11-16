a0058041e16c1326fed74cb88c069e2128e0a244
vnquy126904 <ngocquyvan2005@gmail.com>
2025-11-16 17:18:40 +0700

    [Frontend] Update AuthLayout and route path

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,13 +7,23 @@ import {
 import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/user/HomePage";
 import NotFoundPage from "./pages/shared/NotFoundPage";
+import AuthLayout from "./layouts/AuthLayout";
+import LoginPage from "./pages/auth/LoginPage";
+import RegisterPage from "./pages/auth/RegisterPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<MainLayout />}>
-      <Route index element={<HomePage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Route>
+    <>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+
+      <Route path="auth" element={<AuthLayout />}>
+        <Route path="login" element={<LoginPage />} />
+        <Route path="register" element={<RegisterPage />} />
+      </Route>
+    </>
   )
 );
 
