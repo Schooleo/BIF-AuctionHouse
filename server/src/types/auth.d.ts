a6532a115524a9e74585d2c29709325f30ca3d4a
Schooleo <lnnt190805@gmail.com>
2025-11-17 13:45:26 +0700

    [FIX] Import missing backend-dev files into main

---
+++ server/src/types/auth.d.ts
@@ -0,0 +1,41 @@
+export interface TokenPayload {
+  id: string;
+  role: "bidder" | "seller" | "admin";
+  email: string;
+}
+
+export interface JwtPayload {
+  id: string;
+  role: "bidder" | "seller" | "admin";
+  email: string;
+  iat?: number;
+  exp?: number;
+}
+
+export interface RequestOtpBody {
+  email: string;
+}
+
+export interface RegisterBody {
+  name: string;
+  email: string;
+  password: string;
+  address?: string;
+  otp: string;
+  recaptchaToken: string;
+}
+
+export interface LoginBody {
+  email: string;
+  password: string;
+}
+
+export interface RequestPasswordResetBody {
+  email: string;
+}
+
+export interface ResetPasswordBody {
+  email: string;
+  otp: string;
+  password: string;
+}
