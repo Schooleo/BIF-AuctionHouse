2878df31fd62236d05ec3072586703ac442cdff1
vnquy126904 <ngocquyvan2005@gmail.com>
2025-11-18 10:14:57 +0700

    [Frontend] Update FormCard to suppot mixed field types

--- client/src/components/ui/FormCard.tsx
+++ client/src/components/ui/FormCard.tsx
@@ -1,7 +1,8 @@
-import type { FormCardProps } from "@types/ui";
+import type { FormCardProps, FormField } from "../../types/ui";
 import InputField from "../forms/InputField";
 import PasswordField from "../forms/PasswordField";
 import Button from "../forms/Button";
+import React from "react";
 
 const FormCard: React.FC<FormCardProps> = ({
   title = "Form Pattern",
@@ -20,20 +21,24 @@ const FormCard: React.FC<FormCardProps> = ({
     <form
       onSubmit={handleSubmit}
       className={`
-          bg-white p-8 rounded-xl shadow-lg flex flex-col gap-6
+          bg-white p-8 rounded-xl shadow-lg flex flex-col gap-4
           transition-all duration-300 ease-in-out hover:scale-[1.01] hover:shadow-2xl
           ${className}
       `}
     >
       <h2 className="text-2xl font-semibold">{title}</h2>
-      <div className="bg-white flex flex-col gap-10 mt-10">
-        {fields.map((field, index) =>
-          field.type == "password" ? (
-            <PasswordField key={index} {...field} />
+      <div className="bg-white flex flex-col gap-4">
+        {fields.map((item, i) => {
+          if (React.isValidElement(item)) return <div key={i}>{item}</div>;
+          
+          // Mặc định là FormField khi không phải là React element
+          const field = item as FormField;
+          return field.type === "password" ? (
+            <PasswordField key={i} {...field} />
           ) : (
-            <InputField key={index} {...field} />
-          )
-        )}
+            <InputField key={i} {...field} />
+          );
+        })}
         {children && children}
         <Button className={"w-full mt-5"} {...buttonProps} />
       </div>
