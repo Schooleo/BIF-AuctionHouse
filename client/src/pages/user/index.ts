ab8b6de79d7e03062a10b382c550c76f8de9953c
Schooleo <lnnt190805@gmail.com>
2026-01-04 16:56:51 +0700

    [Deploy] Rename WatchListPage to handle Vercel build error

--- client/src/pages/user/index.ts
+++ client/src/pages/user/index.ts
@@ -2,5 +2,5 @@ export { default as HomePage } from "./HomePage";
 export { default as ProductsPage } from "./ProductsPage";
 export { default as ProductDetailsPage } from "./ProductDetailsPage";
 export { default as ProfilePage } from "./ProfilePage";
-export { default as WatchListPage } from "./WatchListPage";
+export { default as WatchListPage } from "./WatchListPageFinal";
 export { default as BiddingPage } from "./BiddingPage";
