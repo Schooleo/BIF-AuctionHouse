d87a22e9ed0a8a3d9cbb6ee1589f7996856bad54
Schooleo <lnnt190805@gmail.com>
2026-01-04 16:58:07 +0700

    [Deploy] Rename BiddingPage to handle Vercel build error

--- client/src/pages/user/index.ts
+++ client/src/pages/user/index.ts
@@ -3,4 +3,4 @@ export { default as ProductsPage } from "./ProductsPage";
 export { default as ProductDetailsPage } from "./ProductDetailsPage";
 export { default as ProfilePage } from "./ProfilePage";
 export { default as WatchListPage } from "./WatchListPageFinal";
-export { default as BiddingPage } from "./BiddingPage";
+export { default as BiddingPage } from "./BiddingPageFinal";
